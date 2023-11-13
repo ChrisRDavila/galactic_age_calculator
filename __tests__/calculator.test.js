805fb11fa05e60602cca34198d3c3b4d148be768
ChrisRDavila <crossdavila@gmail.com>
2023-11-12 17:12:20 -0800

    test one for age constructor Calculator

---
+++ __tests__/calculator.test.js
@@ -0,0 +1,10 @@
+describe('Calculator', () => {
+  let ageTest;
+  
+  beforeAll(() => {
+    ageTest = new Calculator(38)
+  });
+  test('should return instance of age from age constructor', () => {
+    expect(ageTest.age).toEqual(38);
+  });
+});

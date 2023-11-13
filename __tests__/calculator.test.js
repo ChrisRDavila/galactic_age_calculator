c9261acc466e486323728c82d4e113018c0bc264
ChrisRDavila <crossdavila@gmail.com>
2023-11-12 19:50:21 -0800

    refactor all tests all protoypes to return string responce detailing explanation of answer (as per instructions in assignment)

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -11,59 +11,59 @@ describe('Calculator', () => {
   });
 
   test('#2. should return age in Mercury years', () => {
-    expect(ageTest.mercuryAge()).toEqual(158.33);
+    expect(ageTest.mercuryAge()).toEqual("158.33 mercury years");
   });
 
   test('#3. should return age in Venus years', () => {
-    expect(ageTest.venusAge()).toEqual(61.29);
+    expect(ageTest.venusAge()).toEqual("61.29 venus years");
   });
 
   test('#4. should return age in Mars years', () => {
-    expect(ageTest.marsAge()).toEqual(20.21);
+    expect(ageTest.marsAge()).toEqual("20.21 mars years");
   });
 
   test('#5. should return age in Jupiter years', () => {
-    expect(ageTest.jupiterAge()).toEqual(3.20);
+    expect(ageTest.jupiterAge()).toEqual("3.2 jupiter years");
   });
 
   test('#6. determine earthYears since my 18th birthday', () => {
-    expect(ageTest.earthYearsSince18()).toEqual(20);
+    expect(ageTest.earthYearsSince18()).toEqual("20 earth years have passed");
   });
 
   test('#7. determine mercuryYears since adulthood (18 years old)', () => {
-    expect(ageTest.mercuryYearsSince18()).toEqual(83.33);
+    expect(ageTest.mercuryYearsSince18()).toEqual("83.33 mercury years have passed");
   });
 
   test('#8. determine venusYears since my 18th birthday', () => {
-    expect(ageTest.venusYearsSince18()).toEqual(32.26);
+    expect(ageTest.venusYearsSince18()).toEqual("32.26 venus years have passed");
   });
 
   test('#9. determine marsYears since my 18th birthday', () => {
-    expect(ageTest.marsYearsSince18()).toEqual(10.64);
+    expect(ageTest.marsYearsSince18()).toEqual("10.64 mars years have passed");
   });
 
   test('#10. determine jupiterYears since my 18th birthday', () => {
-    expect(ageTest.jupiterYearsSince18()).toEqual(1.69);
+    expect(ageTest.jupiterYearsSince18()).toEqual("1.69 jupiter years have passed");
   });
 
   test('#11. determine eathYears until retirement (64 earth yearths old)', () => {
-    expect(ageTest.earthYearsUntilRetirement()).toEqual(26);
+    expect(ageTest.earthYearsUntilRetirement()).toEqual("26 earth years will pass");
   })
 
   test('#12. determine mercuryYears until retirement (64 earth yearths old)', () => {
-    expect(ageTest.mercuryYearsUntilRetirement()).toEqual(108.33);
+    expect(ageTest.mercuryYearsUntilRetirement()).toEqual("108.33 mercury years will pass");
   });
 
   test('#13. determine venusYears until retirement (64 earth yearths old)', () => {
-    expect(ageTest.venusYearsUntilRetirement()).toEqual(41.94);
+    expect(ageTest.venusYearsUntilRetirement()).toEqual("41.94 venus years will pass");
   });
 
   test('#14. determine marsYears until retirement (64 earth yearths old)', () => {
-    expect(ageTest.marsYearsUntilRetirement()).toEqual(13.83);
+    expect(ageTest.marsYearsUntilRetirement()).toEqual("13.83 mars years will pass");
   });
 
   test('#15. determine jupiterYears until retirement (64 earth yearths old)', () => {
-    expect(ageTest.jupiterYearsUntilRetirement()).toEqual(2.19);
+    expect(ageTest.jupiterYearsUntilRetirement()).toEqual("2.19 jupiter years will pass");
   });
 });
 
--- src/calculator.js
+++ src/calculator.js
@@ -6,58 +6,58 @@ export class Calculator {
   }
 
   mercuryAge() {
-    return Number.parseFloat((this.earthAge / 0.24).toFixed(2));
+    return Number.parseFloat((this.earthAge / 0.24).toFixed(2)) + " mercury years";
   }
 
   venusAge() {
-    return Number.parseFloat((this.earthAge / 0.62).toFixed(2));
+    return Number.parseFloat((this.earthAge / 0.62).toFixed(2)) + " venus years";
   }
 
   marsAge() {
-    return Number.parseFloat((this.earthAge / 1.88).toFixed(2));
+    return Number.parseFloat((this.earthAge / 1.88).toFixed(2)) + " mars years";
   }
 
   jupiterAge() {
-    return Number.parseFloat((this.earthAge / 11.86).toFixed(2));
+    return Number.parseFloat((this.earthAge / 11.86).toFixed(2)) + " jupiter years";
   }
 
   earthYearsSince18() {
-    return Number.parseFloat((this.earthAge - this.prevAge).toFixed(2));
+    return Number.parseFloat((this.earthAge - this.prevAge).toFixed(2)) + " earth years have passed";
   }
 
   mercuryYearsSince18() {
-    return Number.parseFloat(((this.earthAge - this.prevAge)/0.24).toFixed(2));
+    return Number.parseFloat(((this.earthAge - this.prevAge)/0.24).toFixed(2)) +  " mercury years have passed";
   }
 
   venusYearsSince18() {
-    return Number.parseFloat(((this.earthAge - this.prevAge)/0.62).toFixed(2));
+    return Number.parseFloat(((this.earthAge - this.prevAge)/0.62).toFixed(2)) + " venus years have passed";
   }
 
   marsYearsSince18() {
-    return Number.parseFloat(((this.earthAge - this.prevAge)/1.88).toFixed(2));
+    return Number.parseFloat(((this.earthAge - this.prevAge)/1.88).toFixed(2)) + " mars years have passed";
   }
 
   jupiterYearsSince18() {
-    return Number.parseFloat(((this.earthAge - this.prevAge)/11.86).toFixed(2));
+    return Number.parseFloat(((this.earthAge - this.prevAge)/11.86).toFixed(2)) + " jupiter years have passed";
   }
 
   earthYearsUntilRetirement() {
-    return Number.parseFloat((this.retirementAge - this.earthAge).toFixed(2));
+    return Number.parseFloat((this.retirementAge - this.earthAge).toFixed(2)) + " earth years will pass";
   }
 
   mercuryYearsUntilRetirement() {
-    return Number.parseFloat(((this.retirementAge - this.earthAge)/0.24).toFixed(2));
+    return Number.parseFloat(((this.retirementAge - this.earthAge)/0.24).toFixed(2)) + " mercury years will pass";
   }
 
   venusYearsUntilRetirement() {
-    return Number.parseFloat(((this.retirementAge - this.earthAge)/0.62).toFixed(2));
+    return Number.parseFloat(((this.retirementAge - this.earthAge)/0.62).toFixed(2)) + " venus years will pass";
   }
 
   marsYearsUntilRetirement() {
-    return Number.parseFloat(((this.retirementAge - this.earthAge)/1.88).toFixed(2));
+    return Number.parseFloat(((this.retirementAge - this.earthAge)/1.88).toFixed(2)) + " mars years will pass";
   }
 
   jupiterYearsUntilRetirement() {
-    return Number.parseFloat(((this.retirementAge - this.earthAge)/11.86).toFixed(2));
+    return Number.parseFloat(((this.retirementAge - this.earthAge)/11.86).toFixed(2)) + " jupiter years will pass";
   }
 }
